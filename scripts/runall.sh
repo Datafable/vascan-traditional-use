4b2571894482a84ce3b2d96a72244631a145ca47
Bart Aelterman <bart.aelterman@gmail.com>
2013-09-05 01:33:41 +0200

    Set vascan taxon.txt as argument

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -1,3 +1,5 @@
 # aggregate all lines of one record on one line
+echo "usage: ./scripts/runall.sh /full/path/to/vascan/taxon.txt"
 ./scripts/CleanData.py data/raw/medicinal-plants.tsv data/processed/cleanedData.tsv
 ./scripts/OneLinePerRecord.py ./data/processed/cleanedData.tsv ./data/processed/OneLinePerRecord.csv
+./scripts/MapToVascan.py ./data/processed/OneLinePerRecord.csv $1 ./data/processed/Mapped.txt
