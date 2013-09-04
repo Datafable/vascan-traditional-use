359598969852d78660ec85faef68f3c46191ad2c
Bart Aelterman <bart.aelterman@gmail.com>
2013-09-04 08:21:27 +0200

    add help message

--- scripts/RawDataReader.py
+++ scripts/RawDataReader.py
@@ -4,6 +4,9 @@ import csv
 import sys
 
 def check_arguments():
+    if len(sys.argv) != 3:
+	print "usage: ./RawDataReader.py infile outfile"
+	sys.exit(-1)
     infile, outfile = sys.argv[1:]
     return [infile, outfile]
 
