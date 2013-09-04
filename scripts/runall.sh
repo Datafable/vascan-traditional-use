b57c7ccd82e344b3d72f5f039533a2133d74a077
Bart Aelterman <bart.aelterman@gmail.com>
2013-09-05 01:46:53 +0200

    Add documentation to runall.sh

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -1,5 +1,9 @@
-# aggregate all lines of one record on one line
 echo "usage: ./scripts/runall.sh /full/path/to/vascan/taxon.txt"
+# aggregate all lines of one record on one line
 ./scripts/CleanData.py data/raw/medicinal-plants.tsv data/processed/cleanedData.tsv
+
+# clean some data
 ./scripts/OneLinePerRecord.py ./data/processed/cleanedData.tsv ./data/processed/OneLinePerRecord.csv
-./scripts/MapToVascan.py ./data/processed/OneLinePerRecord.csv $1 ./data/processed/Mapped.txt
+
+# cross map with vascan; the $1 will be filled in by your command line argument and should point to the vascan taxon.txt file
+./scripts/MapToVascan.py ./data/processed/OneLinePerRecord.csv $1 ./data/processed/Mapped.tsv
